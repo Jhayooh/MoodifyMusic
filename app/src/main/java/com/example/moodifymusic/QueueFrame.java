34b0df47fbdb39e05c5a77e5d28c57cee44366e4
Jhayooh <macalinaojhay9@gmail.com>
2023-05-14 21:54:36 +0800

    playlist nav:

---
+++ app/src/main/java/com/example/moodifymusic/QueueFrame.java
@@ -0,0 +1,15 @@
+package com.example.moodifymusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QueueFrame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_queue_frame);
+
+    }
+}
