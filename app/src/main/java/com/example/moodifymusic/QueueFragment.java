826b5482a5e15b9d46040be831d73a7e02e58e36
dstinn <moralesdustin24@gmail.com>
2023-05-28 22:12:38 +0800

    recyclerview

---
+++ app/src/main/java/com/example/moodifymusic/QueueFragment.java
@@ -0,0 +1,125 @@
+package com.example.moodifymusic;
+
+import android.media.Image;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link QueueFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class QueueFragment extends Fragment {
+
+    private RecyclerView musiqqueueRecycler;
+    private List<Music> mMusicData;
+    private MusicItemAdapter musicAdapter;
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public QueueFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment QueueFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static QueueFragment newInstance(String param1, String param2) {
+        QueueFragment fragment = new QueueFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    private void initializeData() {
+        DatabaseReference dbreff = FirebaseDatabase.getInstance().getReference("Music");
+
+        dbreff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mMusicData.clear();
+                for (DataSnapshot musicSnapshot : snapshot.getChildren()) {
+                    Music music = musicSnapshot.getValue(Music.class);
+                    mMusicData.add(music);
+                }
+                musicAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_queue, container, false);
+
+        ImageView close = view.findViewById(R.id.closeQueue);
+        close.setOnClickListener(new ImageView.OnClickListener(){
+            @Override
+            public void onClick(View view1){
+                getActivity().finish();
+            }
+        });
+
+        musiqqueueRecycler = view.findViewById(R.id.mMusicqueueRecycler);
+
+        musiqqueueRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mMusicData = new ArrayList<>();
+
+        musicAdapter = new MusicItemAdapter(this.getContext(), (ArrayList<Music>) mMusicData);
+        musiqqueueRecycler.setAdapter(musicAdapter);
+        initializeData();
+
+        return view;
+    }
+}
